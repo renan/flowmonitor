8ad115668c4cd2e514cc5c75bf94aed4ef688cbc
Daniel Koopmans <daniel@true.nl>
2012-12-13 20:49:30 +0100

    added netflow parser for the future

---
+++ collectors/netflow/version9.js
@@ -0,0 +1,21 @@
+var Collector=require("Netflow");
+var x = new Collector(function (err) {
+    if(err != null) {
+        console.log("ERROR ERROR \n"+err);
+    }
+})
+.on("listening",function() { console.log("listening on 2055"); } )
+
+.on("packet",function(packet) { 
+	var now = new Date();
+	console.log(packet);
+
+//	for (it = 0; it < packet.v5Flows.length; it++) {
+//		console.log(packet.v5Flows[it].srcaddr + ":" + packet.v5Flows[it].srcport  + " -> " + packet.v5Flows[it].dstaddr + ":" + packet.v5Flows[it].dstport + " ( " + packet.v5Flows[it].dOctets + " bytes)"); 
+//	}
+
+} )
+
+
+
+.listen(2055);
