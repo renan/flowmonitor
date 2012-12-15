b728b1b846e38c302c8002ccbf5c6c0e4b64db9e
Renan Gonçalves <renan.saddam@gmail.com>
2012-12-16 00:12:18 +0100

    Response should only have the resource(s), HTTP should be used to return errors.

--- web/backend/lib/resources.js
+++ web/backend/lib/resources.js
@@ -15,23 +15,19 @@ var Crudify = function(model, identifier) {
       query.where[key] = request.query[key];
     }
 
-    model.findAll(query).success(function(results) {
-      var data = {
-        status: 'success',
-        results: results
-      };
-      response.send(JSON.stringify(data));
+    model.findAll(query).success(function(records) {
+      return response.json(records);
     });
   },
 
   // GET /:resource/:id
   show: function(request, response) {
-    model.find(request.params[identifier]).success(function(result) {
-      var data = {
-        status: 'success',
-        result: result
-      };
-      response.send(JSON.stringify(data));
+    model.find(request.params[identifier]).success(function(record) {
+      if (record === null) {
+        return response.json(404, 'Not found');
+      }
+
+      return response.json(record);
     });
   }
  };
